267d652cbd39fcedb731aff798c92c16f46399c1
Khadeja Khalid <KhadejaKhalid@gmail.com>
2019-02-09 14:51:21 -0800

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,45 @@
+
+public class Location 
+{
+	private String name;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	
+	
+	public Location(String n, String a, String c, 
+					String s, String z)
+	{
+		name = n;
+		address = a;
+		city = c;
+		state = s;
+		zip = z;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getCity()
+	{
+		return city;
+	}
+	
+	public String getZip()
+	{
+		return zip;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return (name + "\n" + address + "\n" + city +
+				", " + state + " " + zip + "\n");
+	}
+	
+	
+
+}
