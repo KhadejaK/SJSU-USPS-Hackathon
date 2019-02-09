e57d7f84a96b9c0c63a3d1229999277d36e02509
terryctzeng@gmail.com <terryctzeng@gmail.com>
2019-02-09 15:00:52 -0800

    move locations to usps dir

--- LocationFinderAndTester.java
+++ LocationFinderAndTester.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner; 
+import usps.Location;
 
 public class LocationFinderAndTester 
 {
